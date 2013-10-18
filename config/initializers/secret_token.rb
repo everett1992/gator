ae77134b32a9f39c93e6ea552140eecbe4a52d1f
Caleb Everett <everett.caleb@gmail.com>
2013-10-18 13:14:10 -0400

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gator::Application.config.secret_key_base = 'a1f5b8b492e2909552dc405755fede38003a0f417a7bafc016c27ba1c179a7d7f92b4eaa69ff202d8d13707d11b7e35b517a1ed2e13d21430e9632814f6d3c6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gator::Application.config.session_store :cookie_store, key: '_gator_session'
